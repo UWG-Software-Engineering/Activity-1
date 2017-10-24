634c0a9c0d1dbe9fb1c28364472e1492e3f6f34a
David Jarrett <djarret1@my.westga.edu>
2017-10-24 16:26:22 -0400

    Added testRemoveTodoWhenNoneExist and testRemoveTodoWhenOneExists test methods.

--- TODO App/src/edu/westga/cs3211/todo_app/test/todo_list/TestRemoveTodo.java
+++ TODO App/src/edu/westga/cs3211/todo_app/test/todo_list/TestRemoveTodo.java
@@ -4,11 +4,28 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import edu.westga.cs3211.todo_app.model.Priority;
+import edu.westga.cs3211.todo_app.model.Todo;
+import edu.westga.cs3211.todo_app.model.TodoList;
+
 public class TestRemoveTodo {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testRemoveTodoWhenNoneExist() {
+		TodoList testList = new TodoList();
+		testList.removeTodo(new Todo(Priority.MEDIUM, "Something").getId());
+		assertEquals(0, testList.size());
+	}
+	
+	@Test
+	public void testRemoveTodoWhenOneExists() {
+		TodoList testList = new TodoList();
+		Todo testTodo = new Todo(Priority.HIGH, "Go away");
+		testList.addTodo(testTodo);
+		
+		assertEquals(1, testList.size());
+		testList.removeTodo(testTodo.getId());
+		assertEquals(0, testList.size());
 	}
 
 }
