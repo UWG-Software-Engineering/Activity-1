26c5e6c10746c32772d4899db70d2d0401c2a400
csuser <csuser@160.10.217.89>
2017-10-24 16:35:04 -0400

    tested + created testValidToDoAndList

--- TODO App/src/edu/westga/cs3211/todo_app/test/unsorted_order/TestInsertTodo.java
+++ TODO App/src/edu/westga/cs3211/todo_app/test/unsorted_order/TestInsertTodo.java
@@ -2,13 +2,31 @@ package edu.westga.cs3211.todo_app.test.unsorted_order;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import edu.westga.cs3211.todo_app.model.Priority;
+import edu.westga.cs3211.todo_app.model.Todo;
+import edu.westga.cs3211.todo_app.model.UnsortedOrder;
 
 public class TestInsertTodo {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testNullInsert() {
+		 //TODO
+	}
+	@Test
+	public void testValidToDoAndList() {
+		Todo num = new Todo(Priority.HIGH, "Eat");
+		Todo num2 = new Todo(Priority.MEDIUM, "Eat");
+		Todo num3 = new Todo(Priority.LOW, "Eat");
+		List<Todo> list = new ArrayList<Todo>();
+		UnsortedOrder unsorted = new UnsortedOrder();
+		unsorted.insertTodo(num, list);
+		assertTrue( list.contains(num));
+		
 	}
 
 }
