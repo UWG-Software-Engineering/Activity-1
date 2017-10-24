b4be2b0af99ed1f4e1fb75d56045a50e09a7b171
Tyler Vega & Dallas Wyant <dwyant1@my.westga.edu>
2017-10-24 16:29:00 -0400

    added tests for sortTodo in priority order

--- TODO App/src/edu/westga/cs3211/todo_app/test/priority_order/TestSortTodo.java
+++ TODO App/src/edu/westga/cs3211/todo_app/test/priority_order/TestSortTodo.java
@@ -2,13 +2,26 @@ package edu.westga.cs3211.todo_app.test.priority_order;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
+
+import edu.westga.cs3211.todo_app.model.Priority;
+import edu.westga.cs3211.todo_app.model.PriorityOrder;
+import edu.westga.cs3211.todo_app.model.Todo;
 
 public class TestSortTodo {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testSortWhenOneTodo() {
+		PriorityOrder orderer = new PriorityOrder();
+		List<Todo> todoList = new ArrayList<Todo>();
+		Todo newTodo = new Todo(Priority.LOW, "pick up stuff");
+		orderer.insertTodo(newTodo, todoList);
+		orderer.sortTodos(todoList);
+		assertEquals(todoList, todoList.get(0));
 	}
+	
 
 }
