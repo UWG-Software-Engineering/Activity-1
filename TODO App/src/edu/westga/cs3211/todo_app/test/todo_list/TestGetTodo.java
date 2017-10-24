aa85a3757bdd0cd6e4f6f5bb733627f775572943
csuser <csuser@160.10.217.63>
2017-10-24 16:44:39 -0400

    Completed 2 parameter testing

--- TODO App/src/edu/westga/cs3211/todo_app/test/todo_list/TestGetTodo.java
+++ TODO App/src/edu/westga/cs3211/todo_app/test/todo_list/TestGetTodo.java
@@ -2,13 +2,40 @@ package edu.westga.cs3211.todo_app.test.todo_list;
 
 import static org.junit.Assert.*;
 
+import java.util.UUID;
+
 import org.junit.Test;
+
+import edu.westga.cs3211.todo_app.model.Priority;
+import edu.westga.cs3211.todo_app.model.Todo;
+import edu.westga.cs3211.todo_app.model.TodoList;
 
 public class TestGetTodo {
 
+	/**
+	 * Test getTodos method for proper functionality
+	 */
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testGetPriorityWith2ParamConstructor() {
+		Todo doto = new Todo(Priority.HIGH, "Work");
+		TodoList dotoList = new TodoList();
+	    dotoList.addTodo(doto);
+	    UUID id = doto.getId();
+		assertEquals(doto, dotoList.getTodo(id));
+	}
+
+	/**
+	 * Test getTodos for proper functionality
+	 */
+	@Test
+	public void testGetPriorityWith2ParamConstructor2TODOS() {
+		Todo doto = new Todo(Priority.HIGH, "Work");
+		Todo doto2 = new Todo(Priority.MEDIUM, "Water");
+		TodoList dotoList = new TodoList();
+		UUID id = doto2.getId();
+		dotoList.addTodo(doto);
+		dotoList.addTodo(doto2);
+		assertEquals(doto2, dotoList.getTodo(id));
 	}
 
 }
